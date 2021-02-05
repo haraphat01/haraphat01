40d8d51ba5fc4517057114ec81cad88b11b53934
haraphat01 <haraphat@gmail.com>
2021-02-05 21:29:00 +0100

    added dark mode

---
+++ index.js
@@ -0,0 +1,13 @@
+const options = {
+    bottom: '64px', // default: '32px'
+    right: 'unset', // default: '32px'
+    right: '35px', // default: 'unset'
+    time: '0.9s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#0F2027', // default: '#fff'
+    buttonColorDark: 'rgb(247, 172, 5)', // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: false, // default: true,
+    label: 'Dark Mode', // default: ''
+    autoMatchOsTheme: false // default: true
+}
